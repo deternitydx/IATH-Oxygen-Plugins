21a7e36879f3e73560755dbdab71baeae1d5ae3c
Robbie Hott <robbie@robbiehott.com>
2013-09-30 14:27:48 -0400

    Bugs

--- GetID/src/edu/virginia/iath/oxygenplugins/getid/GetIDPluginMenu.java
+++ GetID/src/edu/virginia/iath/oxygenplugins/getid/GetIDPluginMenu.java
@@ -39,49 +39,49 @@ public class GetIDPluginMenu extends Menu {
 		// Add the menu to select and/or add a new database
 		Menu setupMenu = new Menu("Select Project");
 
-	    // create radio-button style menu for defined document types
-	    ButtonGroup currentDatabases = new ButtonGroup();
-	    JRadioButtonMenuItem currentDBItem;
-	    
-	    for (String project: LocalOptions.getDatabases()) {
-	    	currentDBItem = new JRadioButtonMenuItem(project);
-	    	currentDBItem.setText(project);
-	    	if (project.equals(LocalOptions.getCurrentDB())) {
-	    		currentDBItem.setSelected(true);
-	    	}
-	    	currentDatabases.add(currentDBItem);
-	    }
-	    
-	    setupMenu.addSeparator();
-	    
-	    JMenuItem addNew = new JMenuItem("Add New Project");
-	    addNew.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent selection) {
-                String label = "Add New Project";
+		// create radio-button style menu for defined document types
+		ButtonGroup currentDatabases = new ButtonGroup();
+		JRadioButtonMenuItem currentDBItem;
 
-                JTextField projectURL = new JTextField("http://", 80);
-                JTextField projectName = new JTextField("", 40);
+		for (String project: LocalOptions.getDatabases()) {
+			currentDBItem = new JRadioButtonMenuItem(project);
+			currentDBItem.setText(project);
+			if (project.equals(LocalOptions.getCurrentDB())) {
+				currentDBItem.setSelected(true);
+			}
+			currentDatabases.add(currentDBItem);
+		}
 
-                JPanel addPanel = new JPanel();
-                java.awt.GridLayout layout = new java.awt.GridLayout(2,2); // rows, columns
-                addPanel.setLayout(layout);
-                addPanel.add(new JLabel("Project Name: "));
-                addPanel.add(projectName);
-                addPanel.add(new JLabel("Project ID URL: "));
-                addPanel.add(projectURL);
+		setupMenu.addSeparator();
 
-                int result = JOptionPane.showConfirmDialog((java.awt.Frame)ws.getParentFrame(),
-                    addPanel, label, JOptionPane.OK_CANCEL_OPTION);
+		JMenuItem addNew = new JMenuItem("Add New Project");
+		addNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent selection) {
+				String label = "Add New Project";
 
-                // On OK, store the new database
-                if (result == JOptionPane.OK_OPTION) {
-                	LocalOptions.addDatabase(projectName.getText(), projectURL.getText());
-                }
-            }
-        });
-	    setupMenu.add(addNew);
-	    
-	    this.add(setupMenu);
+				JTextField projectURL = new JTextField("http://", 80);
+				JTextField projectName = new JTextField("", 40);
+
+				JPanel addPanel = new JPanel();
+				java.awt.GridLayout layout = new java.awt.GridLayout(2,2); // rows, columns
+				addPanel.setLayout(layout);
+				addPanel.add(new JLabel("Project Name: "));
+				addPanel.add(projectName);
+				addPanel.add(new JLabel("Project ID URL: "));
+				addPanel.add(projectURL);
+
+				int result = JOptionPane.showConfirmDialog((java.awt.Frame)ws.getParentFrame(),
+						addPanel, label, JOptionPane.OK_CANCEL_OPTION);
+
+				// On OK, store the new database
+				if (result == JOptionPane.OK_OPTION) {
+					LocalOptions.addDatabase(projectName.getText(), projectURL.getText());
+				}
+			}
+		});
+		setupMenu.add(addNew);
+
+		this.add(setupMenu);
 
 
 
@@ -90,8 +90,8 @@ public class GetIDPluginMenu extends Menu {
 
 		// Add the insert ID menu item
 		JMenuItem menuItem = new JMenuItem("Insert Unique ID");
-	    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,
-		        InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,
+				InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		ActionListener action = new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
 				// call something on the action
@@ -109,29 +109,28 @@ public class GetIDPluginMenu extends Menu {
 					selection = ed.getSelectedText();
 				}
 
-				if (selection != null) {
-					// Grab the next ID
-					String nextID = "E0001";
-					
-					// Plug the ID into the result
-					String result = " id=\"" + nextID + "\"";
+				// Grab the next ID
+				String nextID = "E0001";
 
-					// Update the text in the document
-					if (!result.equals(selection)) {
-						ed.beginCompoundUndoableEdit();
-						int selectionOffset = ed.getSelectionStart();
-						ed.deleteSelection();
-						javax.swing.text.Document doc = ed.getDocument();
-						try {
-							doc.insertString(selectionOffset, result,
-									javax.swing.text.SimpleAttributeSet.EMPTY);
-						} catch (javax.swing.text.BadLocationException b) {
-							// Okay if it doesn't work
-						}
-						ed.endCompoundUndoableEdit();
+				// Plug the ID into the result
+				String result = " id=\"" + nextID + "\"";
+
+				// Update the text in the document
+				if (!result.equals(selection)) {
+					ed.beginCompoundUndoableEdit();
+					int selectionOffset = ed.getSelectionStart();
+					ed.deleteSelection();
+					javax.swing.text.Document doc = ed.getDocument();
+					try {
+						doc.insertString(selectionOffset, result,
+								javax.swing.text.SimpleAttributeSet.EMPTY);
+					} catch (javax.swing.text.BadLocationException b) {
+						// Okay if it doesn't work
 					}
-
+					ed.endCompoundUndoableEdit();
 				}
+
+
 
 
 			}
